7e207d3733baeb04a9b94dfd5acf58adcb3920cb
Luis F Marchen <lmarchen@jpl.nasa.gov>
2024-01-16 11:09:58 -0800

    Update makefile_all.sh
    Made slight changes to compiler being used, and includes some of the paths the user needs to set.

--- makefile_all.sh
+++ makefile_all.sh
@@ -4,7 +4,23 @@ set -e
 
 # source intel iccvars file which will be used to compile all libraries
 # source /opt/intel-$intel_version/bin/compilervars.sh intel64
-source /opt/intel/bin/compilervars.sh intel64
+#source /opt/intel/bin/compilervars.sh intel64
+# The following line assumes compilation occurs on s383 Network on mustang4 
+# for a different computer make sure to specify the correct path for setvars.sh
+source /opt/intel-14.0.0/oneapi/setvars.sh intel64 --force
+
+# User must also set paths in order for compilation and macos to run as follows (
+# user can uncomment lines below if user would not like to update the .aliases file):
+# export LD_LIBRARY_PATH=/opt/intel/oneapi/mkl/latest/lib/intel64
+# export LD_LIBRARY_PATH=/home/lmarchen/MACOS/work/git_macos/macos-dev_wrk/macos_f90/pgplot
+# export PGPLOT_FONT=/home/lmarchen/MACOS/work/git_macos/macos-dev_wrk/macos_f90/pgplot/grfont.dat
+# export PGPLOT_DIR=/home/lmarchen/MACOS/work/git_macos/macos-dev_wrk/macos_f90/pgplot
+# export LD_LIBRARY_PATH=/home/lmarchen/MACOS/work/git_macos/macos-dev_wrk/macos_f90/readline-8.2
+# export LD_LIBRARY_PATH=/opt/intel-14.0.0/oneapi/compiler/2023.1.0/linux/compiler/lib/intel64_lin
+
+#--------------------------------------------------------------------------
+# cd to macos source code directory: macos_f90
+#--------------------------------------------------------------------------
 cd macos_f90
 
 #--------------------------------------------------------------------------
@@ -59,31 +75,35 @@ cd ../
 #--------------------------------------------------------------------------
 # Compile macos and smacos
 #--------------------------------------------------------------------------
-#make clean-macos
-#make macos
+make clean-macos
+make macos
 
-#make clean-smacos
-#make smacos
-
-
+make clean-smacos
+make smacos
 
 #--------------------------------------------------------------------------
 # Requirements
 #--------------------------------------------------------------------------
 # Luis used following versions of compilers for compiling macos on "marchen":
 #bash-4.2$ ifort --version
-#ifort (IFORT) 13.1.3 20130607
-#Copyright (C) 1985-2013 Intel Corporation.  All rights reserved.
+#ifort (IFORT) 2021.9.0 20230302
+#Copyright (C) 1985-2023 Intel Corporation.  All rights reserved.
+#or
+#ifx (IFX) 2023.1.0 20230320
+#Copyright (C) 1985-2023 Intel Corporation. All rights reserved.
 
 #bash-4.2$ gcc --version
-#gcc (GCC) 4.8.5 20150623 (Red Hat 4.8.5-39)
-#Copyright (C) 2015 Free Software Foundation, Inc.
+#gcc (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0
+#Copyright (C) 2019 Free Software Foundation, Inc.
 #This is free software; see the source for copying conditions.  There is NO
 #warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 #bash-4.2$ gfortran --version
-#GNU Fortran (GCC) 4.8.5 20150623 (Red Hat 4.8.5-39)
-#Copyright (C) 2015 Free Software Foundation, Inc.
+#GNU Fortran (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0
+#Copyright (C) 2019 Free Software Foundation, Inc.
+#This is free software; see the source for copying conditions.  There is NO
+#warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 
 
 
