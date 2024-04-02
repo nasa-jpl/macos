42411f45dbe374b5c4164601e76700cf00d94d77
sigrist <sigrist@jpl.nasa.gov>
2024-04-01 20:49:35 -0700

    updated pre-release version ID and year

--- macos_f90/macos_definitions.h
+++ macos_f90/macos_definitions.h
@@ -1,3 +1,3 @@
 #define MACOS_ACRONYM       "Modeling and Analysis for Controlled Optical Systems Program (MACOS)"
-#define MACOS_VERSION       '3.35.00'
-#define MACOS_VERSION_YEAR  2023
+#define MACOS_VERSION       '3.36.00'
+#define MACOS_VERSION_YEAR  2024
