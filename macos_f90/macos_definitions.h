f69deed25bb10aeef7cd6f1ca704d8743a99f167
ns <sigrist@jpl.nasa.gov>
2024-05-02 23:30:56 -0700

    Update macos_definitions.h
    Create a new pre-release: 3.36.00 => 3.37.00

--- macos_f90/macos_definitions.h
+++ macos_f90/macos_definitions.h
@@ -1,3 +1,3 @@
 #define MACOS_ACRONYM       "Modeling and Analysis for Controlled Optical Systems Program (MACOS)"
-#define MACOS_VERSION       '3.36.00'
+#define MACOS_VERSION       '3.37.00'
 #define MACOS_VERSION_YEAR  2024
