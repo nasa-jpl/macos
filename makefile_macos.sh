0f57559a3bb42bfb4d007ed0174ac6940f85dd2d
David Redding <62576194+ajax247@users.noreply.github.com>
2026-02-24 18:09:34 -0500

    Clean up ZrnGrData io
    Finishing off the NEW dialog

--- makefile_macos.sh
+++ makefile_macos.sh
@@ -136,7 +136,7 @@ eval $(ifx -C -traceback -fstack-protector -c  -I$macossrc_dir -I$matlab_version
 
 eval $(ifx -C -traceback -fstack-protector -O -shared-intel -shared -Wl,--version-script,$matlab_version/extern/lib/glnxa64/fexport.map  -Wl,--no-undefined -o  "GMI.mexa64"  GMI.o GMIG.o   -Wl,-rpath-link,$matlab_version/bin/glnxa64 -L$matlab_version/bin/glnxa64  -l:libmx.so -l:libmex.so -lmat -L/opt/intel-14.0.0/oneapi/compiler/2023.1.0/linux/compiler/lib/intel64 -lirc -lm -lstdc++  $macossrc_dir/SMACOS_OBJS/Linux-x86_64/smacos_lib.a)
 
-source /opt/intel/oneapi/setvars.sh
+#source /opt/intel/oneapi/setvars.sh
 
 
 #--------------------------------------------------------------------------
